d698ae4f08ae1328990b8127e5050fde0858156e
Anch7703 <anchalkolkar737@gmail.com>
2025-10-22 22:14:21 +0530

    goooo

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
         process.env.NODE_ENV === "production"
-          ? "https://khushijewllers-back.onrender.com/api/auth/google/callback"
+          ? "https://khushijewllers.onrender.com/api/auth/google/callback"
           : "http://localhost:5000/api/auth/google/callback",
     },
     
